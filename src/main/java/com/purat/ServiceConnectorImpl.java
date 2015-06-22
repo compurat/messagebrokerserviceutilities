b8e30d7286623c3098242240b34b864de1b35a52
compurat <pieter.roskam@gmail.com>
2015-06-22 20:14:15 +0200

    abstracted an interface of the connector

---
+++ src/main/java/com/purat/ServiceConnectorImpl.java
@@ -0,0 +1,55 @@
+package com.purat;
+
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.AbstractContentBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by compurat on 20-6-15.
+ */
+@Service
+public class ServiceConnectorImpl implements ServiceConnector {
+
+    private static final String UTF_8 = "UTF-8";
+    private static final Logger LOGGER = Logger.getLogger(ServiceConnectorImpl.class);
+    public String getServiceResponse(final String url, final Map<String, AbstractContentBody> parts) {
+        String response = null;
+        DefaultHttpClient client = new DefaultHttpClient();
+        client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+        HttpPost post = new HttpPost(url);
+        MultipartEntity multipartEntity = createEntity(parts);
+        post.setEntity(multipartEntity);
+        try {
+            response = EntityUtils.toString(client.execute(post).getEntity(), UTF_8);
+        } catch (IOException e) {
+            LOGGER.error("could not access service.");
+            e.printStackTrace();
+        }
+
+        client.getConnectionManager().shutdown();
+        return response;
+
+    }
+
+
+    private MultipartEntity createEntity(final Map<String, AbstractContentBody> parts) {
+        MultipartEntity entity = new MultipartEntity( HttpMultipartMode.BROWSER_COMPATIBLE );
+        Set<String> keys = parts.keySet();
+        for(String key: keys) {
+            entity.addPart(key, parts.get(key));
+        }
+
+        return entity;
+    }
+}
